d9c183986cc2c9e4db62ff07e8f2b4231c6e2c95
th.kim <th.kim@hyperinfo.co.kr>
2023-08-14 16:29:53 +0900

    spring boot tutorial project init

---
+++ spring-boot-tutorial/src/test/java/kr/co/hyperinfo/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hyperinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
